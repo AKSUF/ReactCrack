e40663a3c0eb07ab1dd822f71d18d8746b3c085e
AKSUF <abu053125@gmail.com>
2023-04-29 06:24:30 +0600

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+function Clock(){
+    return(
+<h1 className='heding'>
+<span className='text'>Hello{new Date().toLocaleTimeString()}</span>
+
+</h1>
+
+    )
+}
+
+
+ReactDOM.render(<Clock/>,document.getElementById("root"));
