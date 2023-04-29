e46e582d35ebf7ddee535b6e0961db48dd22dbee
AKSUF <abu053125@gmail.com>
2023-04-29 12:33:33 +0600

    class-props

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-function Clock(){
-    return (
-        <h1 className='heding'>
-            <span className='text'>Hello {new Date().toLocaleTimeString()}</span>
+class Clock extends React.Component {
+    render() {
+      return (
+        <h1 className='heading'>
+          <span className='text'>Hello {new Date().toLocaleTimeString(this.props.locale)}</span>
         </h1>
-    );
-}
+      );
+    }
+  }
+  
+  const ClockComponent = <Clock locale="bn-BD" />;
+  
+  ReactDOM.render(ClockComponent, document.getElementById('root'));
+  
 
-ReactDOM.render(<Clock/>, document.getElementById('root'));
+
+
+//to show time in bengal
+// function Clock({locale}){
+//     return (
+//         <h1 className='heding'>
+//             <span className='text'>Hello {new Date().toLocaleTimeString(locale)}</span>
+//         </h1>
+//     );
+// }
+
+// ReactDOM.render(<Clock locale="bn-BD"/>, document.getElementById('root'));
 
 
