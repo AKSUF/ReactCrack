9404c654fd78ad73b4515ef874aee09fec1bd090
Abu Sufian <121054895+AKSUF@users.noreply.github.com>
2023-04-29 07:20:18 +0600

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-function Clock(){
-    return (
-        <h1 className='heding'>
-            <span className='text'>Hello {new Date().toLocaleTimeString()}</span>
+class Clock extends React.Component {
+    render() {
+      return (
+        <h1 className='heading'>
+          <span className='text'>Hello {new Date().toLocaleTimeString(this.props.locale)}</span>
         </h1>
-    );
-}
-
-ReactDOM.render(<Clock/>, document.getElementById('root'));
+      );
+    }
+  }
+  
+  const ClockComponent = <Clock locale="bn-BD" />;
+  
+  ReactDOM.render(ClockComponent, document.getElementById('root'));
 
 
