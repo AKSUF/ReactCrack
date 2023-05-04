20cbc9b0b4dc7428b22cfeb74beaf2c885a81d27
AKSUF <abu053125@gmail.com>
2023-05-05 04:51:07 +0600

    array in map in react with not keyt index

--- src/component/Clocklist.jsx
+++ src/component/Clocklist.jsx
@@ -3,8 +3,10 @@ import Clock from './Clock';
 function ClookList({quantities=[]}){
     return(
         <div>
-{quantities.map((key)=>(
-    <Clock key={key}/>
+{quantities.map(()=>(
+    // <Clock key={key}/>
+    <Clock key={Math.random()}/>
+
 ))}
         </div>
     );
