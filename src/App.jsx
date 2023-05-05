166117156fc0817bcf88cf6cc1d44a9ce10b0b4c
AKSUF <abu053125@gmail.com>
2023-05-05 07:11:51 +0600

    discussion o form handiling

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Clock from './component/Clock';
 import ClookList from './component/Clocklist';
+import Form from './component/Form';
+
 function App() {
 const quantities=[1,2,3];
 
   return (
     <div className="App">
-   <Clock locale="bn-BD"/>
-<ClookList quantities={quantities} />
-
+  <Form/>
     </div>
   );
 }
